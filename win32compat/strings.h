1a907faf6922404eb96beef344332ae919e346b6
Joel Burton <joel@joelburton.com>
2026-05-03 05:05:05 -0700

    Add MSVC build support for terps
    Three changes that together get 12 of 16 terps compiling under cl.exe:
    - win32compat/{strings.h,unistd.h} mirroring Gargoyle's approach: tiny
      POSIX header shims on the include path for MSVC builds, exposing
      strcasecmp/_stricmp and the F_OK/R_OK/W_OK access() flags.
    - OS_WINDOWS define for glulxe so osdepend.c picks the Win32 branch
      for glulx_malloc/realloc/free.
    - /U_WIN32 on the scare target so scare/os_glk.c skips the WinGlk.h
      Win32-GUI block. Narrower than defining GARGLK, which would also
      activate calls to garglk_set_* extensions remglk doesn't expose.

---
+++ win32compat/strings.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef FLUTTERBUG_WIN32_STRINGS_H
+#define FLUTTERBUG_WIN32_STRINGS_H
+
+#include <string.h>
+
+#define strcasecmp _stricmp
+#define strncasecmp _strnicmp
+
+#endif
---
+++ win32compat/unistd.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef FLUTTERBUG_WIN32_UNISTD_H
+#define FLUTTERBUG_WIN32_UNISTD_H
+
+#include <stdio.h>
+#include <io.h>
+#include <direct.h>
+
+#define F_OK 00
+#define W_OK 02
+#define R_OK 04
+
+#endif
